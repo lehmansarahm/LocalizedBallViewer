817e85f624941f2f6f5e403a82d1bf93f3578a16
Sarah Lehman <tug46038@temple.edu>
2016-09-22 14:07:07 -0400

    Mostly Working First Cut
    Contains working examples of Android plugin integration.  Unity
    instantiates Android-specified interface, then responds to restricted
    area entrance / departure events to change ball color.
    
    Current issue: even though debugging messages confirm entering /
    leaving a restricted area, once the ball turns red, it does not turn
    back to blue.  Working on this…

---
+++ Assets/Scripts/ImageTargetInfo.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class ImageTargetInfo : MonoBehaviour, ITrackableEventHandler {
+	private TrackableBehaviour mTrackableBehaviour;
+
+	void Start() {
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		if (mTrackableBehaviour) {
+			mTrackableBehaviour.RegisterTrackableEventHandler(this);
+		}
+	}
+
+	public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus,
+		TrackableBehaviour.Status newStatus) {
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+			newStatus == TrackableBehaviour.Status.TRACKED ||
+			newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED) {
+			OnTrackingFound();
+		}
+	}
+
+	private void OnTrackingFound() {
+		if (mTrackableBehaviour is ImageTargetAbstractBehaviour) {
+			ImageTargetAbstractBehaviour itb = mTrackableBehaviour as ImageTargetAbstractBehaviour;
+			ImageTarget it = itb.ImageTarget;
+			SetActivityInNativePlugin();
+			ShowTargetInfo(it.Name, it.GetSize().x, it.GetSize().y);
+		}
+	}
+
+	#if UNITY_ANDROID
+	private AndroidJavaObject javaObj = null;
+
+	private AndroidJavaObject GetJavaObject() {
+		if (javaObj == null) {
+			javaObj = new AndroidJavaObject("edu.temple.gamemanager.ImageTargetDetailProvider");
+		}
+		return javaObj;
+	}
+
+	private void SetActivityInNativePlugin() {
+		// Retrieve current Android Activity from the Unity Player
+		AndroidJavaClass jclass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+		AndroidJavaObject activity = jclass.GetStatic<AndroidJavaObject>("currentActivity");
+
+		// Pass reference to the current Activity into the native plugin,
+		// using the 'setActivity' method that we defined in the ImageTargetLogger Java class
+		GetJavaObject().Call("setActivity", activity);
+	}
+
+	private void ShowTargetInfo(string targetName, float targetWidth, float targetHeight) {
+		GetJavaObject().Call("showTargetInfo", targetName, targetWidth, targetHeight);
+	}
+	#else
+	private void SetActivityInNativePlugin() {
+		Debug.Log("SetActivityInNativePlugin method placeholder for Play Mode (not running on Android device)");
+	}
+	private void ShowTargetInfo(string targetName, float targetWidth, float targetHeight) {
+		Debug.Log("ShowTargetInfo method placeholder for Play Mode (not running on Android device)");
+	}
+	#endif
+}
---
+++ Assets/Scripts/RestrictedAreaMonitor.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestrictedAreaMonitor : MonoBehaviour {
+	/// <summary>
+	/// Start this instance.  If environment is Android, instantiate the location tracker, and send it
+	/// an instance of the LocationUpdateListener, attached to the primary sphere object.
+	/// </summary>
+	void Start () {
+		#if UNITY_ANDROID
+		AndroidJavaObject javaObj = new AndroidJavaObject("edu.temple.gamemanager.LocationTracker");
+		MaterialSwapper listener = new MaterialSwapper(GameObject.Find("Sphere"));
+		javaObj.Call("SetLocationUpdateListener", listener);
+		#else
+		Debug.Log("LocationUpdateListener placeholder for Play Mode (not running on Android device)");
+		#endif
+	}
+
+	/// <summary>
+	/// Update this instance.  Currently unused, since all of the event handling is done with the
+	/// LocationUpdateListener class below.
+	/// </summary>
+	void Update () { }
+
+	/// <summary>
+	/// An internal class implementing the basics of the LocationUpdateListener interface.  Responsible
+	/// for storing an instance of a target game object, and toggling its first and second render materials
+	/// when entering or leaving a restricted area, as indicated by the listener events.
+	/// 
+	/// NOTE:  THE UNITY ENVIRONMENT IS NOT RESPONSIBLE FOR DETERMINING WHEN THE USER HAS ENTERED OR
+	/// LEFT A RESTRICTED AREA.  THIS LOGIC IS CONTAINED ENTIRELY WITHIN THE GAME MANAGER ANDROID LIBRARY.
+	/// </summary>
+	class MaterialSwapper : AndroidJavaProxy {
+		private Renderer targetRenderer;
+		private AndroidJavaObject toastDebugger;
+
+		/// <summary>
+		/// Initializes the listener class.  Accepts a game object target for which to update the
+		/// rendered material.
+		/// </summary>
+		/// <param name="target">Target for which to update the rendered material</param>
+		public MaterialSwapper(GameObject target) 
+			: base("edu.temple.gamemanager.LocationUpdateListener") {
+			targetRenderer = target.GetComponent<Renderer>();
+			toastDebugger = new AndroidJavaObject("edu.temple.gamemanager.ToastDebugger");
+			SetActivityInNativePlugin();
+		}
+
+		/// <summary>
+		/// Interface-required method to respond to the user entering a restricted area.  In our
+		/// case, the material for the target will switch to the second in the list (ex: red color)
+		/// </summary>
+		public void onRestrictedAreaEntered() {
+			ShowMessage("Entered a restricted area!");
+			targetRenderer.material = targetRenderer.materials [1];
+		}
+
+		/// <summary>
+		/// Interface-required method to respond to the user leaving a restricted area.  In our 
+		/// case, the material for the target will switch to the first in the list (ex: blue color)
+		/// </summary>
+		public void onRestrictedAreaLeft() {
+			ShowMessage("Left a restricted area!");
+			targetRenderer.material = targetRenderer.materials [0];
+		}
+
+		/// <summary>
+		/// Private internal method to initialize the activity property for our debugger toast maker
+		/// </summary>
+		private void SetActivityInNativePlugin() {
+			AndroidJavaClass jclass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+			AndroidJavaObject activity = jclass.GetStatic<AndroidJavaObject>("currentActivity");
+			toastDebugger.Call("setActivity", activity);
+		}
+
+		/// <summary>
+		/// Private internal method to display a debugger message to the user via Android toast
+		/// </summary>
+		/// <param name="message">Message to display on the toast</param>
+		private void ShowMessage(string message) {
+			toastDebugger.Call("showMessage", message);
+		}
+	}
+}
